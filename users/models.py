bee9d7875a6b380b99a1c307195d8894ba8822c7
Abdulmumin Akinde <lanreywaju97@gmail.com>
2022-01-09 01:15:55 +0100

    set up a customuser with tests

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import CustomUserManager
 
 # Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', max_length=254, unique=True)
+    first_name = models.CharField(verbose_name='first name', max_length=150)
+    last_name = models.CharField(verbose_name='last name', max_length=150)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models,DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
