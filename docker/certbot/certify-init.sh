d016aea7601f02ffcd862d5ebf6a1bf51aba6ec2
Abdulmumin <lanreywaju97@gmail.com>
2023-04-30 15:56:23 +0100

    Add a missing dollar sign

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -14,8 +14,8 @@ echo "Getting certificate..."
 certbot certonly \
     --webroot \
     --webroot-path "/vol/www/" \
+    -d "$DOMAIN" \
     --email "$EMAIL" \
     --rsa-key-size 4096 \
     --agree-tos \
-    --noninteractive \
-    -d "$DOMAIN" \
+    --noninteractive
