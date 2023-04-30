e072ce9ce4dc244a5dd8a4de99da8a0447fcf3d0
Abdulmumin <lanreywaju97@gmail.com>
2023-04-30 23:35:13 +0100

    Adjust certbot config

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -11,11 +11,4 @@ done
 
 echo "Getting certificate..."
 
-certbot certonly \
-    --webroot \
-    --webroot-path "/vol/www/" \
-    -d "$DOMAIN" \
-    --email $EMAIL \
-    --rsa-key-size 4096 \
-    --agree-tos \
-    --noninteractive
+certbot certonly --webroot --webroot-path "/vol/www/" --email $EMAIL --rsa-key-size 4096 --agree-tos --noninteractive -d "$DOMAIN"
