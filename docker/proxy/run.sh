d3e8577998c107754307a9063c17dc0e613fe51b
Abdulmumin <lanreywaju97@gmail.com>
2023-05-01 02:53:46 +0100

    Debug nginx script

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Checking for dhparams.pem..."
-if [ ! -f "/vol/proxy/ssl-dhparams.pem"]; then
-    echo "dhparams.pem does not exist. Creating it..."
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+    echo "dhparams.pem does not exist - creating it"
     openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
 fi
 
