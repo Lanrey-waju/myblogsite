ed2a55b5ecc073e95905882d86013d8cde9de5ad
Abdulmumin <lanreywaju97@gmail.com>
2023-05-12 02:07:50 +0100

    Delete unused code

--- app/blog/views.py
+++ app/blog/views.py
@@ -94,29 +94,6 @@ def post_detail(request, year, month, day, post):
                    'similar_posts': similar_posts})
 
 
-# def post_share(request, post_id):
-#     post = get_object_or_404(Post, id=post_id, status='published')
-#     sent = False
-
-#     if request.method == 'POST':
-#         form = EmailPostForm(request.POST)
-
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             post_url = request.build_absolute_uri(post.get_absolute_url())
-#             subject = f"{cd['name']} recommends you read {post.title}"
-#             message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
-#             send_mail(subject, message, 'mailpytesting@gmail.com', [cd['to'],])
-#             sent = True
-#     else:
-#         form = EmailPostForm()
-#     return render(request, 'blog/post_share.html', {
-#         'post': post,
-#         'form': form,
-#         'sent': sent
-#     })
-
-
 def post_search(request):
     search_form = SearchForm()
     query = None
